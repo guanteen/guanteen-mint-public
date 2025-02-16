02442d2dd87a1817a499a01ede7020a2219b1910
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2025-02-16 21:50:17 +0700

    [+] update

--- settings.tsx
+++ settings.tsx
@@ -1,10 +1,10 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "PUBLIC", mintText: "Join the revolution! Mint your exclusive NFT using SOL.", buttonLabel: "Mint 0.3456 SOL ⚑", header: "🎊 Public Mint (80%)" },
-  { label: "CONTRI", mintText: `Special allocation for talented artists and designers collaborating on Guanteen projects.<br>----------------------<br> Contact: <a href=\"https://twitter.com/guanteen_nft\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color: #1DA1F2; text-decoration: underline;\">@guanteen_nft</a><br>and wait for airdrop🪂`, buttonLabel: "Mint", header: "🎨 CREATIVE CONTRIBUTORS (6%)" },
+  { label: "PUBLIC", mintText: "Join the revolution! Mint your exclusive NFTs.<br>1 NFT = <strong>0.3456 SOL</strong>", buttonLabel: "Mint now ⚑", header: "🎊 Public Mint (80%)" },
+  { label: "CONTRI", mintText: `Special allocation for talented artists and designers collaborating on Guanteen projects.<br>----------------------<br> Contact: <strong><a href=\"https://twitter.com/guanteen_nft\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color: #1DA1F2; text-decoration: underline;\">@guanteen_nft</a></strong><br>and wait for airdrop🪂`, buttonLabel: "Mint", header: "🎨 CREATIVE CONTRIBUTORS (6%)" },
   { label: "COMMU", mintText: "Exclusive drops for our most engaged community members through special events.", buttonLabel: "Mint", header: "🤝 COMMUNITY REWARDS (4%)" },
-  { label: "ADVIS", mintText: `For strategic advisors shaping the future of our project. <br>----------------------<br>Advice us: <a href=\"https://twitter.com/guanteen_nft\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color: #1DA1F2; text-decoration: underline;\">@guanteen_nft</a><br>and wait for airdrop🪂`, buttonLabel: "Mint", header: "👨🏻‍🏫 ADVISOR (2%)" },
+  { label: "ADVIS", mintText: `For strategic advisors shaping the future of our project. <br>----------------------<br>Advice us: <strong><a href=\"https://twitter.com/guanteen_nft\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color: #1DA1F2; text-decoration: underline;\">@guanteen_nft</a></strong><br>and wait for airdrop🪂`, buttonLabel: "Mint", header: "👨🏻‍🏫 ADVISOR (2%)" },
   { label: "TEAM", mintText: "Reserved for our visionary design team who brought this project to life.", buttonLabel: "Mint", header: "👥 FOUNDING TEAM (8%)" },
 ];
 
