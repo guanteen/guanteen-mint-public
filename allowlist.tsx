1b59fe75c0b6f25de8443967c0d6ffff4bebc771
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2025-02-16 14:18:52 +0700

    [+] first commit

---
+++ allowlist.tsx
@@ -0,0 +1,29 @@
+//add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
+//You can also add them here and copy the route from the console log 
+export const allowLists = new Map<string, Array<string>>([
+    ["PUBLIC",
+        []
+    ],
+    ["CONTRI",
+        [
+            "285qV54e3uaVmF8jDp6YroyXRG6mZq3MGWcwNTzookEC",
+        ]
+    ],
+    ["COMMU",
+        [
+            "CyyxHyvAVFDZnuQtcz6kitm2EdGfJs4WWv24x1VR8jTz",
+        ]
+    ],
+    ["ADVIS",
+        [
+            "DhiaFJDm1mcjyqgUmdjkNP5Hpy1z6FJQyZJeS5pB711h",
+        ]
+    ],
+    ["TEAM",
+        [
+            "CCSxBwQ6DzRQjiWTpWxfYaTEoN7k3JwMmxVdtv3y5Ctb",
+            "6ee6jzp5BZgAA3QXmLeJL49hFKhEabgDFwPnXNJMsMjh",
+            "FkjT6zFLd3b8Ra9nJWYtDTQS4ak2bvRCMto6AasPaue2"
+        ]
+    ],
+]);
---
+++ settings.tsx
@@ -0,0 +1,15 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  { label: "PUBLIC", mintText: "Join the revolution! Mint your exclusive NFT using SOL.", buttonLabel: "Mint now ⚑", header: "🎊 Public Mint (80%)" },
+  { label: "CONTRI", mintText: "Special allocation for talented artists and designers collaborating on guaranteed projects.", buttonLabel: "Mint", header: "🎨 CREATIVE CONTRIBUTORS (6%)" },
+  { label: "COMMU", mintText: "Exclusive drops for our most engaged community members through special events.", buttonLabel: "Mint", header: "🤝 COMMUNITY REWARDS (4%)" },
+  { label: "ADVIS", mintText: "For strategic advisors shaping the future of our project.", buttonLabel: "Mint", header: "👨🏻‍🏫 ADVISORY MINT (2%)" },
+  { label: "TEAM", mintText: "Reserved for our visionary design team who brought this project to life.", buttonLabel: "Mint", header: "👥 FOUNDING TEAM (8%)" },
+];
+
+//header image in the ui. replace with your own
+export const image = "https://gateway.irys.xyz/cSlTxyxRGnQksCPn8qo6DWNBgLYnKYipl7xLnyHmVi4?ext=png";
+
+//website title
+export const headerText = "Guanteen NFT 🤪🦶🏻";
